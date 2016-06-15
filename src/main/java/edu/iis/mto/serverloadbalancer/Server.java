baa0f622211ef470674149659d0df502fe245f63
MichalakJ <daran.dasaner@gmail.com>
2016-06-15 17:50:39 +0200

    test3 refactor

--- src/main/java/edu/iis/mto/serverloadbalancer/Server.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/Server.java
@@ -4,6 +4,11 @@ package edu.iis.mto.serverloadbalancer;
  * Created by Kuba on 2016-06-15.
  */
 public class Server {
+    public static final double MAXIMUM_LOAD = 100.0d;
     public double currentLoadPercentage;
     public int capacity;
+
+    public void addVm(Vm vm) {
+        currentLoadPercentage =  (double) vm.size * MAXIMUM_LOAD / (double) capacity;
+    }
 }
