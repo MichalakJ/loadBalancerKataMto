86f89b471067c95ff899dfc9807d2111b84662ea
MichalakJ <daran.dasaner@gmail.com>
2016-06-15 20:31:54 +0200

    test1 refactor

---
+++ src/main/java/edu/iis/mto/serverloadbalancer/CurrentLoadPercentageMatcher.java
@@ -0,0 +1,36 @@
+package edu.iis.mto.serverloadbalancer;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+
+/**
+ * Created by Kuba on 2016-06-15.
+ */
+public class CurrentLoadPercentageMatcher extends TypeSafeMatcher<Server> {
+    public static final double EPSILON = 0.01d;
+    private double expectedLoadPercentage;
+
+    public CurrentLoadPercentageMatcher(double expectedLoadPercentage) {
+        this.expectedLoadPercentage = expectedLoadPercentage;
+    }
+
+    protected boolean matchesSafely(Server server) {
+        double d1 = server.loadPercentage;
+        double d2 = this.expectedLoadPercentage;
+        return isDoubleEqual(d1, d2);
+    }
+
+    private boolean isDoubleEqual(double d1, double d2) {
+        return d1 == d2 || Math.abs(d2 - d1) < EPSILON;
+    }
+
+    public void describeTo(Description description) {
+        description.appendText("server with load percentage of ").appendValue(expectedLoadPercentage);
+    }
+
+    @Override
+    protected void describeMismatchSafely(Server item, Description mismatchDescription) {
+        mismatchDescription.appendText("server with load percentage of ").appendValue(item.loadPercentage);
+    }
+}
--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -4,6 +4,7 @@ package edu.iis.mto.serverloadbalancer;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
+import org.hamcrest.Matcher;
 import org.junit.Test;
 
 public class ServerLoadBalancerTest {
@@ -15,14 +16,27 @@ public class ServerLoadBalancerTest {
     @Test
     public void balancingServer_noVms_serverStaysEmpty(){
         Server server = new Server();
-        Vm[] emptyVmList = new Vm[0];
-        Server[] serverList = new Server[] {server};
-        new ServerLoadBalancer().balance(serverList, emptyVmList);
 
-        assertThat(server.loadPercentage, equalTo(0.0d));
+        balance(serverListWith(server), emptyVmList());
+
+        assertThat(server, hasLoadPercentageOf(0.0d));
     }
 
+    private void balance(Server[] servers, Vm[] vms) {
+        new ServerLoadBalancer().balance(servers,vms);
+    }
 
+    private Vm[] emptyVmList(Vm... vms) {
+        return vms;
+    }
+
+    private Server[] serverListWith(Server... servers) {
+        return servers;
+    }
+
+    private Matcher<Server> hasLoadPercentageOf(double expectedLoadPercentage) {
+        return new CurrentLoadPercentageMatcher(expectedLoadPercentage);
+    }
 
 
 }
